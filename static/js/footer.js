bffd4bdd60da5f91f70d144d50bfb14ec655b6b1
EyyubAmiraslan <eyyubamiraslan3625@gmail.com>
2020-10-29 17:12:58 +0400

    static folderi footer assetsdir

---
+++ static/js/footer.js
@@ -0,0 +1,11 @@
+function myFunction() {
+    document.getElementById("img_container_img").style.display = "none";
+    document.getElementById("message_box_id").style.display = "block";
+      }
+
+function MySecondFunction() {
+    document.getElementById("img_container_img").style.display = "block";
+    document.getElementById("message_box_id").style.display = "none";
+      }
+
+    
